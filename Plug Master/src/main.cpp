d1fb99b76bde2edc4ac7a6ed6b35fdb06b1a7282
Tyler-Fanuele <tfanuele@gmail.com>
2023-07-09 21:46:17 -0400

    Added comments to ease readability

--- Plug Master/src/main.cpp
+++ Plug Master/src/main.cpp
@@ -1,12 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
+// Message struct with string for message
 typedef struct struct_message {
   String msg;
 } struct_message;
 
+// Holds teh mac address of the slave of the system
+// This will be changed to accept a list of mac addresses from a
+// serial port later to allow for many slaves
 uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0xB9, 0x4E, 0x2E};
 
+// This is the on send function for the master
 void OnDataSent(uint8_t* mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
@@ -19,6 +24,8 @@ void OnDataSent(uint8_t* mac_addr, uint8_t sendStatus) {
 
 void setup(){
   Serial.begin(115200);
+  
+  // Init wifi
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != 0) {
@@ -26,7 +33,9 @@ void setup(){
     return;
   }
 
+  // Init esp-now and set role
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  // Register a on send function
   esp_now_register_send_cb(OnDataSent);
   
   // Register peer
@@ -34,6 +43,7 @@ void setup(){
 }
  
 void loop(){
+  // Wait for serial input
   while (Serial.available() == 0) {} 
   String incomingString = Serial.readString();
   incomingString.trim();
@@ -41,13 +51,16 @@ void loop(){
   Serial.println("Received Command '" + incomingString + "':");
 
   if (incomingString == "mac") {
+    // 'mac' was seen on serial and will now print mac address
     Serial.print("ESP8266 Board MAC Address:  ");
     Serial.println(WiFi.macAddress());
   } else if (incomingString == "send") {
+    // 'send' was seen on serial and will send a message over esp-now
     struct_message myData;
     myData.msg = "in master";
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   } else {
+    // error case, unrecognised command seen over serial
     Serial.println("Command: '" + incomingString+ "' is not recognised!");
   }
 }
--- Plug Slave/src/main.cpp
+++ Plug Slave/src/main.cpp
@@ -1,13 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
+// Message struct with string for message
 typedef struct struct_message {
   String msg;
 } struct_message;
 
-
+// Holds mac address of master
 uint8_t broadcastAddress[] = {0x48, 0x55, 0x19, 0xDF, 0x29, 0xAD};
 
+// This is the receive function for the slave
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   struct_message myData;
   memcpy(&myData, incomingData, sizeof(myData) );
@@ -16,7 +18,9 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
 
 void setup(){
   Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
+
+  // Init wifi
+  WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
   if (esp_now_init() != 0) {
@@ -31,7 +35,7 @@ void setup(){
 }
  
 void loop(){
-  /**/
+  // Wait for serial
   while (Serial.available() == 0) {} 
   String incomingString = Serial.readString();
   incomingString.trim();
@@ -39,9 +43,11 @@ void loop(){
   Serial.println("Received Command '" + incomingString + "':");
 
   if (incomingString == "mac") {
+    // 'mac' seen on serial, print mac address
     Serial.print("ESP8266 Board MAC Address:  ");
     Serial.println(WiFi.macAddress());
   } else {
+    // error case, unrecognised command seen over serial
     Serial.println("Command: '" + incomingString+ "' is not recognised!");
   }
 }
