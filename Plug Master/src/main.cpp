7dc756c621c637759fb09697da9985762b3fabfb
Tyler-Fanuele <tfanuele@gmail.com>
2023-07-09 21:39:35 -0400

    Added esp-now testing

--- Plug Master/src/main.cpp
+++ Plug Master/src/main.cpp
@@ -1,7 +1,36 @@
 #include <ESP8266WiFi.h>
+#include <espnow.h>
+
+typedef struct struct_message {
+  String msg;
+} struct_message;
+
+uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0xB9, 0x4E, 0x2E};
+
+void OnDataSent(uint8_t* mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
 
 void setup(){
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
  
 void loop(){
@@ -14,6 +43,10 @@ void loop(){
   if (incomingString == "mac") {
     Serial.print("ESP8266 Board MAC Address:  ");
     Serial.println(WiFi.macAddress());
+  } else if (incomingString == "send") {
+    struct_message myData;
+    myData.msg = "in master";
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   } else {
     Serial.println("Command: '" + incomingString+ "' is not recognised!");
   }
--- Plug Slave/src/main.cpp
+++ Plug Slave/src/main.cpp
@@ -1,10 +1,37 @@
 #include <ESP8266WiFi.h>
+#include <espnow.h>
+
+typedef struct struct_message {
+  String msg;
+} struct_message;
+
+
+uint8_t broadcastAddress[] = {0x48, 0x55, 0x19, 0xDF, 0x29, 0xAD};
+
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+  struct_message myData;
+  memcpy(&myData, incomingData, sizeof(myData) );
+  Serial.println(myData.msg);
+}
 
 void setup(){
   Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
 }
  
 void loop(){
+  /**/
   while (Serial.available() == 0) {} 
   String incomingString = Serial.readString();
   incomingString.trim();
